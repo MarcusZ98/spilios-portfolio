7fb47aaeb793ba14865fc5483a7cc95b4a5bd37a
MarcusZ98 <marcus.zibung@gmail.com>
2026-02-27 21:31:03 +0100

    Improve loading

--- js/shader.js
+++ js/shader.js
@@ -6,60 +6,57 @@ async function init() {
 
   const containers = document.querySelectorAll(".showcase-shader");
 
-  const { vertex, fragment } = await loadShaderSource("background");
-
   containers.forEach(container => {
-
-    const scene = new THREE.Scene();
-    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-
-    const renderer = new THREE.WebGLRenderer({
-      alpha: true,
-      premultipliedAlpha: false
-    });
-
-    renderer.setClearColor(0x000000, 0);
-    renderer.setPixelRatio(window.devicePixelRatio);
-
-    container.appendChild(renderer.domElement);
-
-    const uniforms = {
-      iTime: { value: 0 },
-      iResolution: { value: new THREE.Vector2() },
-      iMouse: { value: new THREE.Vector2() }
-    };
-
-    const material = new THREE.ShaderMaterial({
-      uniforms,
-      vertexShader: vertex,
-      fragmentShader: fragment,
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false
-    });
-
-    const geometry = new THREE.PlaneGeometry(2, 2);
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    function resize() {
-      const rect = container.getBoundingClientRect();
-      renderer.setSize(rect.width, rect.height, false);
-      uniforms.iResolution.value.set(rect.width, rect.height);
-    }
-
-    resize();
-    window.addEventListener("resize", resize);
-
-    instances.push({
-      scene,
-      camera,
-      renderer,
-      uniforms
-    });
+    createInstance(container);
   });
 
   animate();
+}
+
+async function createInstance(container) {
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+
+  const renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    premultipliedAlpha: false
+  });
+
+  renderer.setClearColor(0x000000, 0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  container.appendChild(renderer.domElement);
+
+  const uniforms = {
+    iTime: { value: 0 },
+    iResolution: { value: new THREE.Vector2() }
+  };
+
+  const { vertex, fragment } = await loadShaderSource("background");
+
+  const material = new THREE.ShaderMaterial({
+    uniforms,
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    transparent: true,
+    blending: THREE.AdditiveBlending,
+    depthWrite: false
+  });
+
+  const geometry = new THREE.PlaneGeometry(2, 2);
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  function resize() {
+    const rect = container.getBoundingClientRect();
+    renderer.setSize(rect.width, rect.height, false);
+    uniforms.iResolution.value.set(rect.width, rect.height);
+  }
+
+  resize();
+  window.addEventListener("resize", resize);
+
+  instances.push({ scene, camera, renderer, uniforms });
 }
 
 async function loadShaderSource(name) {
